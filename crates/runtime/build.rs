2e651271303b812a1d18f2e2e6029ce56749e56c
Kevin Boos <kevinaboos@gmail.com>
2023-08-02 18:03:19 -0700

    Fix wasmtime-runtime build of `helpers.c` on macOS hosts for Theseus

--- crates/runtime/build.rs
+++ crates/runtime/build.rs
@@ -11,6 +11,25 @@ fn main() {
         .file("src/helpers.c");
     
         if env::var("CARGO_CFG_TARGET_OS").unwrap().contains("theseus") {
+            // When building Theseus on a macOS host machine,
+            // we need to use the "x86_64-elf-" cross compiler toolchain,
+            // and also tell that compiler where the sysroot include directory is
+            // such that it can find the required header files.
+            if env::var("HOST").unwrap().contains("darwin") {
+                let xcrun_output = std::process::Command::new("xcrun")
+                    .arg("--show-sdk-path")
+                    .output()
+                    .expect("Failed to run \"xcrun --show-sdk-path\"")
+                    .stdout;
+                let sdk_path = String::from_utf8(xcrun_output).unwrap();
+                let include_path = format!("{}/usr/include", sdk_path.trim());
+                build
+                    .compiler("x86_64-elf-gcc")
+                    .archiver("x86_64-elf-ar")
+                    .ranlib("x86_64-elf-ranlib")
+                    .include(include_path);
+            }
+
             build
                 .use_plt(false)
                 .pic(false)
@@ -24,7 +43,9 @@ fn main() {
                 // Disable stack smashing protection
                 .flag("-fno-stack-protector");
 
-                // eprintln!("Theseus build: {:?}\n{:?}", build, build.get_compiler());
+                // eprintln!("Theseus build: {:#?}\n{:#?}", build, build.get_compiler());
+                // eprintln!("Theseus build command: {:?}", build.get_compiler().to_command());
+                // eprintln!("Theseus $(HOST)={:?}", env::var("HOST"));
         }
 
     build.compile("wasmtime-helpers");
